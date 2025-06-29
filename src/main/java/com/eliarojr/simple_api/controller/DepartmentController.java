76181508957308e67c44725a0f666159ae5cdd79
Enock Masakhwe <enockmasakhwe@gmail.com>
2025-06-29 15:45:26 +0300

    Successfully implemented all the crud functionalities

--- src/main/java/com/eliarojr/simple_api/controller/DepartmentController.java
+++ src/main/java/com/eliarojr/simple_api/controller/DepartmentController.java
@@ -4,9 +4,9 @@ package com.eliarojr.simple_api.controller;
 import com.eliarojr.simple_api.entity.Department;
 import com.eliarojr.simple_api.service.DepartmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class DepartmentController {
@@ -17,5 +17,32 @@ public class DepartmentController {
     @PostMapping("/departments")
     public Department saveDepartment(@RequestBody Department department){
         return departmentService.saveDepartment(department);
+    }
+
+    @GetMapping("/departments")
+    public List<Department> fetchDepartmentList(){
+        return departmentService.fetchDepartmentList();
+    }
+
+    @GetMapping("/departments/{id}")
+    public Department fetchDepartmentById(@PathVariable("id") Long departmentId){
+        return departmentService.fetchDepartmentById(departmentId);
+    }
+
+    @GetMapping("/departments/name/{name}")
+    public Department fetchDepartmentByName(@PathVariable("name")String departmentName){
+        return departmentService.fetchDepartmentByName(departmentName);
+    }
+
+    @DeleteMapping("/departments/{id}")
+    public String deleteDepartmentById(@PathVariable("id")Long departmentId){
+        departmentService.deleteDepartmentById(departmentId);
+        return "Department deleted successfully!";
+    }
+
+    @PutMapping("/departments/{id}")
+    public Department updateDepartment (@RequestBody Department department,
+                                        @PathVariable("id") Long departmentId){
+        return departmentService.updateDepartment(department, departmentId);
     }
 }
