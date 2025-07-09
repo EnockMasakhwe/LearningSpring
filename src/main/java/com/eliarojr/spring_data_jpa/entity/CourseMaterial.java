907a514a19bce394e01ca82aae53a1dde5adc772
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-07-09 11:11:20 +0300

    Added cascading and fetch types

--- src/main/java/com/eliarojr/spring_data_jpa/entity/CourseMaterial.java
+++ src/main/java/com/eliarojr/spring_data_jpa/entity/CourseMaterial.java
@@ -1,16 +1,14 @@
 package com.eliarojr.spring_data_jpa.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@ToString(exclude = "course")
 public class CourseMaterial {
 
     @Id
@@ -26,7 +24,10 @@ public class CourseMaterial {
     private Long courseMaterialId;
     private String url;
 
-    @OneToOne
+    @OneToOne(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
     @JoinColumn(
             name = "course_id",
             referencedColumnName = "courseId"
