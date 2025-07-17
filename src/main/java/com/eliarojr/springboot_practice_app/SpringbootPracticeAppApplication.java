0b90c271be74c83c1e913297b18cd86d72dca7f9
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-07-17 16:09:38 +0300

    implemented the first simple api

---
+++ src/main/java/com/eliarojr/springboot_practice_app/SpringbootPracticeAppApplication.java
@@ -0,0 +1,13 @@
+package com.eliarojr.springboot_practice_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootPracticeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootPracticeAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eliarojr/springboot_practice_app/SpringbootPracticeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eliarojr.springboot_practice_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPracticeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
