3f0e47eada8693d29b88f7302903de3798f4b93a
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-07-28 11:34:04 +0300

    Add config in application.properties file

--- springboot-practice-app/src/main/java/com/eliarojr/springboot_practice_app/controller/GreetingsController.java
+++ springboot-practice-app/src/main/java/com/eliarojr/springboot_practice_app/controller/GreetingsController.java
@@ -1,12 +1,16 @@
 package com.eliarojr.springboot_practice_app.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingsController {
 
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     @GetMapping("/")
     public String Greetings(){
-        return "Hey techies, am just having fun with Springboot:)";
+        return welcomeMessage;
     }
 }
