5bfb2b370b5accdf47b8b87bae06046bd3e4a7b0
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-07-30 17:29:18 +0300

    Implement paging and sorting

--- spring-data-jpa-practice/src/main/java/com/eliarojr/spring_data_jpa_practice/repository/CourseRepository.java
+++ spring-data-jpa-practice/src/main/java/com/eliarojr/spring_data_jpa_practice/repository/CourseRepository.java
@@ -1,9 +1,13 @@
 package com.eliarojr.spring_data_jpa_practice.repository;
 
 import com.eliarojr.spring_data_jpa_practice.entity.Course;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository <Course, Long> {
+
+    Page<Course> findByTitleContaining(String title, PageRequest pageRequest);
 }
--- spring-data-jpa-practice/src/test/java/com/eliarojr/spring_data_jpa_practice/repository/CourseRepositoryTest.java
+++ spring-data-jpa-practice/src/test/java/com/eliarojr/spring_data_jpa_practice/repository/CourseRepositoryTest.java
@@ -2,10 +2,15 @@ package com.eliarojr.spring_data_jpa_practice.repository;
 
 import com.eliarojr.spring_data_jpa_practice.entity.Course;
 import com.eliarojr.spring_data_jpa_practice.entity.Teacher;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -29,7 +34,7 @@ class CourseRepositoryTest {
                 .lastName("Curry")
                 .build();
 
-        Course sql = Course.builder()
+        /*Course sql = Course.builder()
                 .title("SQL")
                 .credit(7)
                 .teacher(teacher)
@@ -39,6 +44,7 @@ class CourseRepositoryTest {
                 .credit(6)
                 .teacher(teacher)
                 .build();
+         */
         Course mariaDB = Course.builder()
                 .title("MariaDB")
                 .credit(7)
@@ -46,6 +52,42 @@ class CourseRepositoryTest {
                 .build();
 
         courseRepository.save(mariaDB);
+    }
+    
+    @Test
+    public void findAllPagination(){
+        PageRequest firstPageWithThreeRecords = PageRequest.of(0,3);
+        PageRequest secondPageWithTwoRecords = PageRequest.of(1,2);
+        
+        Page <Course> page = courseRepository.findAll(secondPageWithTwoRecords);
+
+        long totalElements = page.getTotalElements();
+        int totalPages = page.getTotalPages();
+        List<Course> courseList = page.getContent();
+
+        System.out.println("totalPages = " + totalPages);
+        System.out.println("totalElements = " + totalElements);
+        System.out.println("courses = " + courseList);
+        
+    }
+
+    @Test
+    public void findAllSorting(){
+        PageRequest sortByTitle = PageRequest.of(0,2, Sort.by("title"));
+        PageRequest sortByCreditDesc = PageRequest.of(0,2,Sort.by("credit").descending());
+        PageRequest sortByTitleAndCreditDesc = PageRequest.of(0,2,Sort.by("title").descending().and(Sort.by("credit")));
+
+        List <Course> courses = courseRepository.findAll(sortByTitle).getContent();
+        System.out.println("courses = " + courses);
+    }
+
+    @Test
+    public void printFindByTitleContaining(){
+        PageRequest firstPageTenRecords = PageRequest.of(0,10);
+
+        List<Course> courses = courseRepository.findByTitleContaining("M",firstPageTenRecords).getContent();
+
+        System.out.println("courses = " + courses);
     }
 
 
