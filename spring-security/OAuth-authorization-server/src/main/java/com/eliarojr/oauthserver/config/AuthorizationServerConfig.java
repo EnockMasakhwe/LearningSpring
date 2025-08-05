5f9c0432a5767bf877bdc56360d9d3168e4edaa1
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-08-05 13:01:38 +0300

    Add authorization-server, security filter chain, client registration configuration

---
+++ spring-security/OAuth-authorization-server/src/main/java/com/eliarojr/oauthserver/config/AuthorizationServerConfig.java
@@ -0,0 +1,76 @@
+package com.eliarojr.oauthserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+import java.util.UUID;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    //Default authserver security filter chain
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer = new OAuth2AuthorizationServerConfigurer();
+
+        http
+                .securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, (configurer) -> configurer
+                        .oidc(withDefaults()) // Enable OpenID Connect 1.0 if needed
+                )
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                )
+                .formLogin(withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    //Default basic client registration configuration
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("api-client")
+                .clientSecret(passwordEncoder.encode("secret"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                //redirect urls
+                .redirectUri("http://127.0.0.1:8080/login/oauth2/code/api-client-oidc")
+                .redirectUri("http://127.0.0.1:8080/authorized")
+                //scopes
+                .scope(OidcScopes.OPENID) // Required for OpenID Connect
+                .scope("api.read")
+                .clientSettings(ClientSettings.builder()
+                        .requireAuthorizationConsent(true) // Requires user consent for scopes
+                        .build())
+                .build();
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+
+
+}
