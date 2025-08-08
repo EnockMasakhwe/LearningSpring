e122f21618e48490df445d4b949c698aab4e0ee6
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-08-08 13:09:34 +0300

    Call the web client from the HelloController class

--- spring-security/OAuth-resource-server/src/main/java/com/eliarojr/OAuth_resource_server/controller/UserController.java
+++ spring-security/OAuth-resource-server/src/main/java/com/eliarojr/OAuth_resource_server/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.eliarojr.OAuth_resource_server.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
+    @GetMapping("/api/users")
     public String [] getUser(){
         return new String[]{"Enock","Ian","Benard"};
     }
--- spring-security/spring-security-client/src/main/java/com/eliarojr/spring_security_client/controller/HelloController.java
+++ spring-security/spring-security-client/src/main/java/com/eliarojr/spring_security_client/controller/HelloController.java
@@ -1,16 +1,38 @@
 package com.eliarojr.spring_security_client.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.security.Principal;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction.*;
 
 @RestController
 public class HelloController {
 
+    @Autowired
+    private WebClient webClient;
     //Simple API
     @GetMapping("/api/hello")
     public String helloMessage(Principal principal){
         return "Hello " + principal.getName() + ", welcome to my Security app!";
+    }
+
+    //Use webclient to call the resource-server api
+    @GetMapping("/api/users")
+    public String [] users(@RegisteredOAuth2AuthorizedClient("api-client-authorization-code")
+                               OAuth2AuthorizedClient client){
+    return webClient
+            .get()
+            .uri("http://localhost:8090/api/users")
+            .attributes(oauth2AuthorizedClient(client))
+            .retrieve()
+            .bodyToMono(String[].class)
+            .block();
     }
 }
