3b2f8127020614313d337dfde48f8fbc68f64a8b
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-08-08 12:20:42 +0300

    Add Principal method to HelloController to see logged in user details in the app output

--- spring-security/spring-security-client/src/main/java/com/eliarojr/spring_security_client/controller/HelloController.java
+++ spring-security/spring-security-client/src/main/java/com/eliarojr/spring_security_client/controller/HelloController.java
@@ -3,12 +3,14 @@ package com.eliarojr.spring_security_client.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HelloController {
 
     //Simple API
     @GetMapping("/api/hello")
-    public String helloMessage(){
-        return "Hello, welcome to my Security app!";
+    public String helloMessage(Principal principal){
+        return "Hello " + principal.getName() + ", welcome to my Security app!";
     }
 }
