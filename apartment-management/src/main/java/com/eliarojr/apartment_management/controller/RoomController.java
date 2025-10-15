2dabc165dd75de9f4df0319cce43be9208f08ee9
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-10-15 15:22:28 +0300

    Implement hibernate validation and Loggers

--- apartment-management/src/main/java/com/eliarojr/apartment_management/controller/RoomController.java
+++ apartment-management/src/main/java/com/eliarojr/apartment_management/controller/RoomController.java
@@ -2,6 +2,9 @@ package com.eliarojr.apartment_management.controller;
 
 import com.eliarojr.apartment_management.entity.Room;
 import com.eliarojr.apartment_management.service.RoomService;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,34 +16,42 @@ public class RoomController {
     @Autowired
     public RoomService roomService;
 
+    private final Logger LOGGER = LoggerFactory.getLogger(RoomController.class);
+
     @PostMapping("/rooms")
-    public Room saveRoom(@RequestBody Room room){
+    public Room saveRoom(@Valid @RequestBody Room room){
+        LOGGER.info("Inside saveRoom of RoomController");
         return roomService.saveRoom(room);
     }
 
     @GetMapping("/rooms")
     public List<Room> fetchRoomList(){
+        LOGGER.info("Inside fetchRoomList of RoomController");
         return roomService.fetchRoomList();
     }
 
     @GetMapping("/rooms/id/{id}")
     public Room fetchRoomById(@PathVariable("id")Long roomId){
+        LOGGER.info("Inside fetchRoomById of RoomController");
         return roomService.fetchRoomById(roomId);
     }
 
     @GetMapping("/rooms/name/{name}")
     public Room fetchRoomByName(@PathVariable("name") String roomName){
+        LOGGER.info("Inside fetchRoomByName of RoomController");
         return roomService.fetchRoomByName(roomName);
     }
 
     @DeleteMapping("/rooms/id/{id}")
     public String deleteRoomById(@PathVariable("id") Long roomId){
         roomService.deleteRoomById(roomId);
+        LOGGER.info("Inside deleteRoomById of RoomController");
         return "Room deleted successfully!!";
     }
 
     @PutMapping("/rooms/id/{id}")
     public Room updateRoom(@PathVariable("id")Long roomId, @RequestBody Room room){
+        LOGGER.info("Inside updateRoom of RoomController");
         return roomService.updateRoom(roomId,room);
     }
 
