fccb347205289529a9d012cbf97b771cacb819cf
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-10-21 13:53:27 +0300

    Implement unit testing for service layer

---
+++ apartment-management/src/test/java/com/eliarojr/apartment_management/service/RoomServiceTest.java
@@ -0,0 +1,55 @@
+package com.eliarojr.apartment_management.service;
+
+import com.eliarojr.apartment_management.entity.Room;
+import com.eliarojr.apartment_management.error.RoomNotFoundException;
+import com.eliarojr.apartment_management.repository.RoomRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class RoomServiceTest {
+
+    @Autowired
+    private RoomService roomService;
+
+    @MockitoBean
+    private RoomRepository roomRepository;
+
+    @BeforeEach
+    void setUp() {
+        Room room = Room.builder()
+                .roomName("2A")
+                .roomAddress("Vision")
+                .roomCode("VS-12")
+                .roomId(1L)
+                .build();
+        Mockito.when(roomRepository.findById(1L)).thenReturn(Optional.ofNullable(room));
+    }
+
+    @Test
+    @DisplayName("Get data based on valid room name")
+    public void whenValidRoomName_thenRoomShouldFound(){
+        String roomName = "2A";
+        Room found = roomService.fetchRoomByName(roomName);
+
+        assertEquals(roomName, found.getRoomName());
+    }
+
+    @Test
+    @DisplayName("Get data based on valid room id")
+    public void whenValidRoomId_thenRoomShouldFound() throws RoomNotFoundException {
+        Long roomId = 1L;
+        Room found = roomService.fetchRoomById(roomId);
+
+        assertEquals(roomId, found.getRoomId());
+    }
+}
