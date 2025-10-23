67b41c78c148ed19776980227e74bd9e5ba1bb0b
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-10-23 16:44:39 +0300

    Implement embeddable and embedded annotations

--- college-database/src/test/java/com/eliarojr/college_database/repository/StudentRepositoryTest.java
+++ college-database/src/test/java/com/eliarojr/college_database/repository/StudentRepositoryTest.java
@@ -1,5 +1,6 @@
 package com.eliarojr.college_database.repository;
 
+import com.eliarojr.college_database.entity.Guardian;
 import com.eliarojr.college_database.entity.Student;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,12 +20,30 @@ class StudentRepositoryTest {
                 .firstName("Raila")
                 .lastName("Odinga")
                 .emailId("raila@gmail,com")
-                .guardianName("Jaramogi Oginga")
-                .guardianEmail("jaramogi@gmail.com")
-                .guardianMobile("0756438712")
+//                .guardianName("Jaramogi Oginga")
+//                .guardianEmail("jaramogi@gmail.com")
+//                .guardianMobile("0756438712")
                 .build();
         studentRepository.save(student);
 
+    }
+
+    @Test
+    public void saveStudentWithGuardian(){
+        Guardian guardian = Guardian.builder()
+                .name("Raila Odinga")
+                .email("raila@gmail.com")
+                .mobile("0897654367")
+                .build();
+
+        Student student = Student.builder()
+                .firstName("Raila")
+                .lastName("Junior")
+                .emailId("junior@gmail.com")
+                .guardian(guardian)
+                .build();
+
+       studentRepository.save(student);
     }
 
     @Test
