25d37ec777882ae4970b632e17b5d1a18970a23a
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-10-29 09:40:19 +0300

    Implement one to one bidirectional mapping

--- college-database/src/test/java/com/eliarojr/college_database/repository/CourseRepositoryTest.java
+++ college-database/src/test/java/com/eliarojr/college_database/repository/CourseRepositoryTest.java
@@ -1,10 +1,24 @@
 package com.eliarojr.college_database.repository;
 
+import com.eliarojr.college_database.entity.Course;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class CourseRepositoryTest {
+    
+    @Autowired
+    private CourseRepository courseRepository;
+    
+    @Test
+    public void printAllCourses(){
+        List<Course> courseList = courseRepository.findAll();
+        System.out.println("courseList = " + courseList);
+    }
 
 }
