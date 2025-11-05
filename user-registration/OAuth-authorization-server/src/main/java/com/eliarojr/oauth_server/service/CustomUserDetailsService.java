6c183793039c152080e3913935ea1b9419da4229
EnockMasakhwe <enockmasakhwe@gmail.com>
2025-11-05 16:47:16 +0300

    Implement project set up for OAuth-authorization-server

---
+++ user-registration/OAuth-authorization-server/src/main/java/com/eliarojr/oauth_server/service/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.eliarojr.oauth_server.service;
+
+import com.eliarojr.oauth_server.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return null;
+    }
+}
