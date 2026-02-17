456b69f6934aa6f4e9ced2fc9db33e4e74b358e0
EnockMasakhwe <enockmasakhwe@gmail.com>
2026-02-17 11:54:50 +0300

    Initial commit for the springboot project

---
+++ spring-concepts-revision/demo/src/main/java/com/eliarojr/demo/SpringbootRevisionApplication.java
@@ -0,0 +1,13 @@
+package com.eliarojr.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRevisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRevisionApplication.class, args);
+	}
+
+}
