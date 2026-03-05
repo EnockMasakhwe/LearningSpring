e20bf86b73aea352fa460dfa47902987468eb483
EnockMasakhwe <enockmasakhwe@gmail.com>
2026-03-05 15:24:49 +0300

    Created and implemented CustomUserDetailsService,CustomAuthenticationProvider,AuthrorizationServerConfig and DefaultSecurityConfig classes

---
+++ spring-security-revision/OAuth-authorization-server/src/main/java/com/eliarojr/oauth_server/config/AuthorizationServerConfig.java
@@ -0,0 +1,139 @@
+package com.eliarojr.oauth_server.config;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.authorization.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.util.UUID;
+
+@Configuration(proxyBeanMethods = false)
+public class AuthorizationServerConfig {
+
+    @Autowired
+    private  PasswordEncoder passwordEncoder;
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    //Default security filter chain config
+    public SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
+        // Apply OAuth2 authorization server configuration
+        OAuth2AuthorizationServerConfigurer authorizationServerConfigurer =
+                new OAuth2AuthorizationServerConfigurer();
+
+        // Apply the default security configuration for the authorization server
+        http.securityMatcher(authorizationServerConfigurer.getEndpointsMatcher())
+                .with(authorizationServerConfigurer, (configurer) ->
+                        configurer.oidc(oidc -> oidc
+                                .providerConfigurationEndpoint(provider -> provider
+                                        .providerConfigurationCustomizer(customizer -> {
+                                            customizer.issuer("http://auth-server:9000");
+                                        })
+                                )
+                        )
+                )
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().authenticated()
+                );
+
+        // Add form login for the authorization server endpoints
+        return http
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    //Default registered client repository config
+    public RegisteredClientRepository registeredClientRepository(PasswordEncoder passwordEncoder) {
+        RegisteredClient registeredClient = RegisteredClient.withId(UUID.randomUUID().toString())
+                .clientId("api-client")
+                .clientSecret(passwordEncoder.encode("secret"))
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+                .redirectUri("http://localhost:8088/login/oauth2/code/api-client-oidc")
+                .redirectUri("http://localhost:8088/authorized")
+                .scope(OidcScopes.OPENID)
+                .scope("api.read")
+                .clientSettings(ClientSettings.builder().requireAuthorizationConsent(true).build()) // Pass the ClientSettings object
+                //.requireAuthorizationConsent(true) // Requires user consent for scopes
+                .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+    //Client settings method
+    /*private ClientSettings clientSettings() {
+        return ClientSettings.builder()
+                .requireProofKey(true) // Enable PKCE for enhanced security
+                .requireAuthorizationConsent(true) // Requires user consent for scopes
+                .build();
+    }
+     */
+
+    @Bean
+    //Default configs for public and private keys
+    public JWKSource<SecurityContext> jwkSource(){
+        RSAKey rsaKey = generateRsa();
+        JWKSet jwkSet = new JWKSet(rsaKey);
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    private static RSAKey generateRsa() {
+        KeyPair keyPair = generateRsaKey();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        return new RSAKey.Builder(publicKey)
+                .privateKey(privateKey)
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    private static KeyPair generateRsaKey() {
+        KeyPair keyPair;
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            keyPair = keyPairGenerator.generateKeyPair();
+        } catch (Exception ex) {
+            throw new IllegalStateException(ex);
+        }
+        return keyPair;
+    }
+
+    @Bean
+    //Provider settings config
+    public AuthorizationServerSettings authorizationServerSettings() {
+        return AuthorizationServerSettings.builder()
+                .issuer("http://auth-server:9000")
+                .authorizationEndpoint("/oauth2/authorize")  // Explicitly set endpoints
+                .tokenEndpoint("/oauth2/token")
+                .jwkSetEndpoint("/oauth2/jwks")
+                .oidcClientRegistrationEndpoint("/connect/register")
+                .oidcUserInfoEndpoint("/userinfo")
+                .build();
+    }
+}
