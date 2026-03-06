10a85462c5b0752636d3b5bd3828f48d912887a3
EnockMasakhwe <enockmasakhwe@gmail.com>
2026-03-06 16:05:31 +0300

    Used Principal class to implement getting logged in user's name

--- spring-security-revision/client/src/main/java/com/eliarojr/client/controller/HelloController.java
+++ spring-security-revision/client/src/main/java/com/eliarojr/client/controller/HelloController.java
@@ -3,12 +3,14 @@ package com.eliarojr.client.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String helloWorld(){
-        return "Hello World! Welcome to my SpringBoot App!";
+    public String helloWorld(Principal principal){
+        return "Hello" + principal.getName() + ", Welcome to my SpringBoot App!";
     }
 }
 
